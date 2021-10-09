f836aacd49e0c574bb9d2784ae589398b751017a
Christian Sosa <chrissm28@outlook.com>
2021-10-09 02:55:32 -0500

    Homework 04 Terminada

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -223,21 +223,18 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
-  var array = [];
-  var suma = numero;
-  for(var i= 0; i<10; i++) {
-    suma = suma + 2;
-    if(suma === i) break;
-    else {
-      array.push(suma);
+  var nuevoArray = [];
+  for(var i=0;i<10;i++){
+    numero+=2;
+    if(numero===i){
+      break;
     }
+    nuevoArray.push(numero);
   }
-  if(i < 10) {
-    return 'Se interrumpió la ejecución';
-  }
-  else {
-      return array;
-  }
+  if(i<10)
+    return "Se interrumpió la ejecución";
+  else
+    return nuevoArray;
 }
 
 
@@ -248,16 +245,15 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
-  var array = [];
-  var suma = numero;
-  for(var i= 0; i<10; i++) {
-    if(i === 5) continue;
-    else {
-      suma = suma + 2;
-      array.push(suma);
+  var nuevoArray = [];
+  for(var i=0;i<10;i++){
+    if(i===5){
+      continue;
     }
+    numero+=2;
+    nuevoArray.push(numero);
   }
-  return array;
+  return nuevoArray; 
 }
 
 
