695016f9a3e0e951ea7d13d005a5a04925eddedd
Christian Sosa <chrissm28@outlook.com>
2021-10-11 19:41:23 -0500

    Añadida Homework Tema 7

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -51,8 +51,7 @@ function map(array, cb) {
   return nuevoArray;
 }
 
-
-function filter(array) {
+function filter(array){
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
